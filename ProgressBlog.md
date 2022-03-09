de2d097b6ab1674e1d0553774bfb427b6aff7776
Mattiwos <bmattiwos@gmail.com>
2022-03-09 01:35:39 +0000

    started working on database types

---
+++ ProgressBlog.md
@@ -0,0 +1,6 @@
+## mar 8 2022  
+17:34 PST  
+I started working on creating 
+Database types so I can determine
+which add/remove function to use.  
+
--- include/structDB.h
+++ include/structDB.h
@@ -1,7 +1,13 @@
+
+typedef enum typeDatabases {
+  KV_DB, DO_DN, HYBRID_DO
+} typeDB;
+
 struct Database {
-    char name[69];
-    int space;
-    int id;
+    char name[69];//Database name
+    int space;//Amount of Space database uses
+    int id;//Random ID
+    typeDB type;
     char location[];
 };
 struct Table {
