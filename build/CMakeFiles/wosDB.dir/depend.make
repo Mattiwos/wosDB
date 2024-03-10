c4981e3615b793f5ebd05cd9deaf1e3b50f606a8
mattiwos <bmattiwos@gmail.com>
2024-03-09 23:07:38 -0800

    removed build file

--- build/CMakeFiles/wosDB.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for wosDB.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mattiwosbelachew/Repos/github.com/mattiwos/wosDB
-# Build directory: /Users/mattiwosbelachew/Repos/github.com/mattiwos/wosDB/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
